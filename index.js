8b70fef1905e8dddce348c4306e05893406793e4
Deian Mishev <deian.mishev@bulpros.com>
2019-05-17 14:35:43 +0300

    Moving out day

--- index.js
+++ index.js
@@ -2,58 +2,37 @@ console.log("===========================");
 console.log("======= BUILD CHECK =======");
 console.log("===========================");
 console.log("==== SERVICE ENV VARS =====");
-console.log("WF_BE_ADDRESS: " + process.env.WF_BE_ADDRESS);
+console.log("BE_ADDRESS: " + process.env.BE_ADDRESS);
 console.log("PORT: " + process.env.PORT);
 console.log("===========================");
 
-const path = require("path");
-const axios = require("axios");
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const resolvedBase = path.join(`${__dirname}/src`);
-const resolvedIndex = path.join(`${__dirname}/src/index.html`);
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(resolvedBase));
+app.use(cookieParser());
+
+app.get("/pageSegmentation/:page", (req, res) => {
+  const page = req.params.page;
+  // Add segmentation from mongo
+  res.send([page]);
+});
+
+app.post("/link", (req, res) => {
+  const params = req.body;
+  // Add entry to
+  // var randomNumber=Math.random().toString();
+  // randomNumber=randomNumber.substring(2,randomNumber.length);
+  // res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+  res.cookie("username", "Flavio");
+  res.send(params);
+});
+
 app.use(cors());
-
-const request_options = req => {
-  delete req.headers.origin;
-  delete req.headers.referer;
-  return {
-    method: req.method,
-    data: req.body,
-    url: `${process.env.WF_BE_ADDRESS}/${req.headers.proxy_url}`,
-    headers: req.headers
-  };
-};
-
-const proxy = (req, frontendResponse) => {
-  axios(request_options(req))
-    .then(backendResponse => {
-      frontendResponse.send(backendResponse.data);
-    })
-    .catch(error => {
-      if (error.response && error.response.data && error.response.data.error) {
-        frontendResponse.send({ error: error.response.data.error });
-        return;
-      }
-      frontendResponse.send(error);
-    });
-};
-
-app.get("/pageSegmentation", (req, res) => {
-  res.send("GET/pageSegmentation");
-});
-
-app.post("/position", (req, res) => {
-  res.send("POST/position");
-});
-
 app.listen(process.env.PORT);
 
 console.log(`Service listening on port ${process.env.PORT}`);
