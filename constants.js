bacf596e87f7fee35ff8729bd926bfa41395b530
LeadShuriken <d.s.mishev@gmail.com>
2019-11-10 23:52:54 +0100

    Yeap (

--- constants.js
+++ constants.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 8080;
 const API_URL = process.env.API_URL || '/api';
 const STATIC_SERVE = process.env.SERVETYPE === 'STATIC';
 const RESOURCES = path.join(__dirname, 'resources/');
-const RSA_PRIVATE_KEY = fs.readFileSync(`${__dirname}/private.key`);
-const RSA_PUBLIC_KEY = fs.readFileSync(`${__dirname}/public.key`);
+// const RSA_PRIVATE_KEY = fs.readFileSync(`${__dirname}/private.key`);
+// const RSA_PUBLIC_KEY = fs.readFileSync(`${__dirname}/public.key`);
 const JWT_ALGORITHM = 'RS256';
 
 const HTML = path.join(__dirname, 'public/');
@@ -18,8 +18,8 @@ module.exports = {
     PORT,
     API_URL,
     STATIC_SERVE,
-    RSA_PRIVATE_KEY,
-    RSA_PUBLIC_KEY,
+    // RSA_PRIVATE_KEY,
+    // RSA_PUBLIC_KEY,
     JWT_ALGORITHM,
     HTML,
     RESOURCES,
