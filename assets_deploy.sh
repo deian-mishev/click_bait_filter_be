e53d2ca91d70a4aac469116d44b992b8e2e7c606
LeadShuriken <d.s.mishev@gmail.com>
2020-06-09 16:39:25 +0200

    Heroku Deploy

--- assets_deploy.sh
+++ assets_deploy.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
-
-# brew install gnupg
-# cat turtles.csv \
-# | gpg --armor --symmetric --output - \
-# | curl -X PUT --upload-file "-" https://transfer.sh/turtles.csv \
-# | pbcopy
-
-
-# RSA KEYS
-PRIVATE_KEY_URL=$(cat ./keys/private.key | curl -X PUT --upload-file "-" https://transfer.sh/private.key)
-PUBLIC_KEY_URL=$(cat ./keys/public.key | curl -X PUT --upload-file "-" https://transfer.sh/public.key)
-
-# MODEL
-MODEL_JSON=$(cat ./model/model.json | curl -X PUT --upload-file "-" https://transfer.sh/model.json)
-MODEL_BIN=$(cat ./model/group1-shard1of1.bin | curl -X PUT --upload-file "-" https://transfer.sh/group1-shard1of1.bin)
-MODEL_MAPPING=$(cat ./keys/mapping.json | curl -X PUT --upload-file "-" https://transfer.sh/mapping.json)
-
-# TRANSFER
-# curl {PRIVATE_KEY_URL} --output ./keys/private.key
-# curl https://transfer.sh/abOWI/private.key --output ./keys/private.key
-# curl https://transfer.sh/abOWI/private.key --output ./keys/private.key
-# curl https://transfer.sh/abOWI/private.key --output ./keys/private.key
+git checkout heroku
+git pull --rebase origin develop
+git push heroku origin:master
+git checkout develop
--- constants.js
+++ constants.js
@@ -4,18 +4,8 @@ const PORT = process.env.PORT || 4000;
 const API_URL = process.env.API_URL || '/api';
 const TOKEN_EXPIRATION = process.env.TOKEN_EXPIRATION || 999999;
 const TENSOR_DIMENSIONS = process.env.TENSOR_DIMENSIONS || 3500;
-
-let temp_pr = '';
-let temp_pb = '';
-try {
-    temp_pr = fs.readFileSync(`${__dirname}/keys/private.key`);
-    temp_pb = fs.readFileSync(`${__dirname}/keys/public.key`);
-}
-catch (err) {
-}
-
-const RSA_PRIVATE_KEY = temp_pr;
-const RSA_PUBLIC_KEY = temp_pb;
+const RSA_PRIVATE_KEY = fs.readFileSync(`${__dirname}/keys/private.key`);
+const RSA_PUBLIC_KEY = fs.readFileSync(`${__dirname}/keys/public.key`);
 const JWT_ALGORITHM = 'RS256';
 
 module.exports = {
