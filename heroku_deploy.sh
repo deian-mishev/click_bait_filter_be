361a59bae2f7649bbd44b27484da6d07605399f3
LeadShuriken <d.s.mishev@gmail.com>
2020-06-11 20:31:31 +0200

    Deploy update

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 git checkout heroku
+cp -r ./../click_bait_filter_ml/model/* ./model/
+git add .
+git commit -m 'Model Update and Deploy'
 git pull --rebase origin develop
 git push heroku heroku:master
 git checkout develop
